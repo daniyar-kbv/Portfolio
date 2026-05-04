e83a298eaff17b7f13a743fe3d94e6d49a96267e
Daniyar Kurmanbayev <daniyar.kbv@gmail.com>
2026-05-04 14:44:30 +0500

    refactor: add typography prose styling

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,3 +1,5 @@
+import typography from '@tailwindcss/typography';
+
 export default {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,ts,tsx,vue}'],
   theme: {
@@ -16,5 +18,5 @@ export default {
       },
     },
   },
-  plugins: [],
+  plugins: [typography],
 };
